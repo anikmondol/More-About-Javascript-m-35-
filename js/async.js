da5fafd14ced354d53ca04867c3ef8b60bbd4438
anikmondol <anikmondol558363@gmil.com>
2024-04-28 21:34:27 +0600

    this vidoe about single-threaded in javascrpti, asynchrous vs synchronous

---
+++ js/async.js
@@ -0,0 +1,24 @@
+console.log(1);
+console.log(2);
+
+// console.log(3);
+// doSomething();
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+// setTimeout(doSomething);
+// setTimeout(doSomething, 2000);
+setTimeout(() => {
+    console.log('anik akon acay');
+}, 2000);
+
+console.log(4);
+console.log(5);
+console.log(6);
+
+
+function doSomething(){
+    console.log(3);
+}
